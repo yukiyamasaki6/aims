a1a0c8b8d07f06a533ecf848007de3e6170058e5
yukiyamasaki6 <184247188+yukiyamasaki6@users.noreply.github.com>
2026-07-25 02:03:04 +0900

    feat(web): shdcn/uiを導入

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
       <h1 className="text-2xl font-bold">AIMS Frontend</h1>
+      <Button>shadcn/ui Button Test</Button>
     </main>
   );
 }
